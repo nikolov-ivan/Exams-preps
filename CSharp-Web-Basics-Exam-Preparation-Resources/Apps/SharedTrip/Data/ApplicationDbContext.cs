aef2a77c133cc03f97ed8c9fa98564fbc6d03c67
ivan nikolov <nikolov.iv@gmail.com>
2020-10-24 14:53:21 +0300

    Initial commit

---
+++ CSharp-Web-Basics-Exam-Preparation-Resources/Apps/SharedTrip/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿namespace SharedTrip.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions db)
+            : base(db)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server = DESKTOP-K2OHBCV\SQLEXPRESS; Database = TripsShared; Integrated Security = true");
+            }
+        }
+    }
+}
